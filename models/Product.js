0cdace52ae7dc30a22c5695eb9e4274fdc338e90
ForestW70 <hexaforest@gmail.com>
2021-06-16 17:33:28 -0400

    created sequelize tables for all of the models. added env variables.

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,42 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true,
+      defaultValue: 10,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+
   },
   {
     sequelize,
