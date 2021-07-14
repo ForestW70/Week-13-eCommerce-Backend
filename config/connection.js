67e65a23156037e083486968a42476243eac173e
ForestW70 <hexaforest@gmail.com>
2021-07-14 14:09:10 -0400

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: process.env.DB_PORT,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
